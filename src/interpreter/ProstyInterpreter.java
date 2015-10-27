fe0d1cc6505593d48d2b22dc54307388617408bf
mateusz-gnomer <mateusz_gnomer@o2.pl>
2015-10-27 21:18:26 +0100

    Implementacja podstawowych komend i testów interpretera

--- src/interpreter/ProstyInterpreter.java
+++ src/interpreter/ProstyInterpreter.java
@@ -3,28 +3,36 @@
  */
 package interpreter;
 
+//import java.util.List;
+//import java.util.ArrayList;
+
 import model.StanMaszyny;
 /**
  * @author gn
  *
  */
-public class ProstyInterpreter implements Interpreter {
+public class ProstyInterpreter implements Interpreter{
     private StanMaszyny stanMaszyny;
     private int wartoscZawinieta;
-    
+//    private int licznikZagniezdzenia;
+    // private List<Integer> pozycjeOtwierajace;
     
     public ProstyInterpreter(StanMaszyny stanMaszyny){
         this.stanMaszyny = stanMaszyny;
         this.wartoscZawinieta = 255;
+ //       this.licznikZagniezdzenia = 0;
+        
+        //this.pozycjeOtwierajace = new ArrayList<Integer>();
     }
     
     public void krok(){
         int temp;
+        char znak;
         char komenda = this.stanMaszyny.aktualnaKomenda();
         if(komenda == 0){
             return;
         }
-        this.stanMaszyny.ustawPozycjeKodu(this.stanMaszyny.pozycjaKodu()+1);
+
         switch(komenda){
         
         case '<':
@@ -33,6 +41,7 @@ public class ProstyInterpreter implements Interpreter {
                 temp = this.stanMaszyny.dlugoscTasmy()-1;
             }
             this.stanMaszyny.ustawPozycjeTasmy(temp);
+            this.stanMaszyny.ustawPozycjeKodu(this.stanMaszyny.pozycjaKodu()+1);
             break;
             
         case '>':
@@ -41,6 +50,7 @@ public class ProstyInterpreter implements Interpreter {
                 temp = 0;
             }
             this.stanMaszyny.ustawPozycjeTasmy(temp);
+            this.stanMaszyny.ustawPozycjeKodu(this.stanMaszyny.pozycjaKodu()+1);
             break;
             
         case '+':
@@ -49,6 +59,7 @@ public class ProstyInterpreter implements Interpreter {
                 temp = 0;
             }
             this.stanMaszyny.ustawWartoscTasmy(temp);
+            this.stanMaszyny.ustawPozycjeKodu(this.stanMaszyny.pozycjaKodu()+1);
             break;
             
         case '-':
@@ -57,10 +68,80 @@ public class ProstyInterpreter implements Interpreter {
                 temp = this.wartoscZawinieta;
             }
             this.stanMaszyny.ustawWartoscTasmy(temp);
+            this.stanMaszyny.ustawPozycjeKodu(this.stanMaszyny.pozycjaKodu()+1);
+            break;
+        case '.':
+            znak = (char) this.stanMaszyny.wartoscTasmy();
+            this.stanMaszyny.ustawWyjscie(
+                    this.stanMaszyny.zwrocWyjscie() + znak
+            );
+            this.stanMaszyny.ustawPozycjeKodu(this.stanMaszyny.pozycjaKodu()+1);
+            break;
+        case ',':
+            if(this.stanMaszyny.zwrocWejscie().length()
+                    >this.stanMaszyny.pozycjaWejscia()){
+                znak  = this.stanMaszyny.zwrocWejscie().
+                        charAt(this.stanMaszyny.pozycjaWejscia()
+                );
+                this.stanMaszyny.ustawPozycjeWejscia(
+                        this.stanMaszyny.pozycjaWejscia()+1
+                );
+            } else{
+                znak = 0;
+            }
+            this.stanMaszyny.ustawWartoscTasmy((int) znak);
+            this.stanMaszyny.ustawPozycjeKodu(this.stanMaszyny.pozycjaKodu()+1);
+            break;
+        case '[':
+            if(this.stanMaszyny.wartoscTasmy()==0){
+                przejdzDoZamykajacego();
+            }    
+            this.stanMaszyny.ustawPozycjeKodu(this.stanMaszyny.pozycjaKodu()+1);
+            break;
+            
+        case ']':
+            wrocDoOtwierajacego();
+            break;
+        default:
             break;
         }
         
+        
     }
+    
+    private void dodajOtwierajacy(){
+
+        //this.pozycjeOtwierajace.add(this.stanMaszyny.pozycjaKodu());
+    }
+    private void przejdzDoZamykajacego(){
+        int temp = 1;
+        while(temp != 0){
+            this.stanMaszyny.ustawPozycjeKodu(
+                    this.stanMaszyny.pozycjaKodu()+1);
+            if(this.stanMaszyny.aktualnaKomenda()=='['){
+                temp++;
+            }
+            if(this.stanMaszyny.aktualnaKomenda()==']'){
+                temp--;
+            }
+        }
+
+    }
+    private void wrocDoOtwierajacego(){
+        int temp = 1;
+        while(temp != 0){
+            this.stanMaszyny.ustawPozycjeKodu(
+                    this.stanMaszyny.pozycjaKodu()-1);
+            if(this.stanMaszyny.aktualnaKomenda()=='['){
+                temp--;
+            }
+            if(this.stanMaszyny.aktualnaKomenda()==']'){
+                temp++;
+            }
+        }
+
+    }
+    
     public void uruchomDoPunktu(){
         
     }
--- testy/interpreter/TestInterpretera.java
+++ testy/interpreter/TestInterpretera.java
@@ -97,5 +97,120 @@ public class TestInterpretera {
         }
     }
     
+    @Test
+    public void testKrokKomenda5(){
+        this.stanMaszyny.ustawKod("..");
+        this.stanMaszyny.ustawWartoscTasmy(65);
+        
+        this.interpreter.krok();
+        assertTrue("A".equals(this.stanMaszyny.zwrocWyjscie()));  
+        
+        this.interpreter.krok();
+        assertTrue("AA".equals(this.stanMaszyny.zwrocWyjscie()));  
+    }
+    
+    @Test
+    public void testKrokKomenda6(){
+        this.stanMaszyny.ustawKod(",,,");
+        this.stanMaszyny.ustawWejscie("Aa");
+        
+        this.interpreter.krok();
+        assertTrue(this.stanMaszyny.wartoscTasmy() == 65);
+        assertTrue(this.stanMaszyny.pozycjaWejscia() == 1);
+        
+        this.interpreter.krok();
+        assertTrue(this.stanMaszyny.wartoscTasmy() == 97);
+        assertTrue(this.stanMaszyny.pozycjaWejscia() == 2);
+        
+        this.interpreter.krok();
+        assertTrue(this.stanMaszyny.wartoscTasmy() == 0);
+        assertTrue(this.stanMaszyny.pozycjaWejscia() == 2);
+    }
+    
+    @Test
+    public void testPetla1(){
+        this.stanMaszyny.ustawKod("[>]");
+        
+        this.stanMaszyny.ustawWartoscTasmy(1);
+        
+        this.stanMaszyny.ustawPozycjeTasmy(1);
+        this.stanMaszyny.ustawWartoscTasmy(1);
+        
+        this.stanMaszyny.ustawPozycjeTasmy(2);
+        this.stanMaszyny.ustawWartoscTasmy(1);
+
+        this.stanMaszyny.ustawPozycjeTasmy(0);
+        //          taśma      kod
+        //pozycja   0 1 2 3    0 1 2 3
+        //wartosc   1 1 1 0    [ > ]
+        //wskaznik  ^          ^
+        
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        
+        assertTrue(this.stanMaszyny.pozycjaTasmy() == 3);
+        assertTrue(this.stanMaszyny.pozycjaKodu() == 0);
+    }
+    
+    @Test 
+    public void testPetlaZagniezdzona(){
+        this.stanMaszyny.ustawKod("[[-]>]");
+        
+        this.stanMaszyny.ustawWartoscTasmy(1);
+        
+        this.stanMaszyny.ustawPozycjeTasmy(1);
+        this.stanMaszyny.ustawWartoscTasmy(1);
+        
+        this.stanMaszyny.ustawPozycjeTasmy(2);
+        this.stanMaszyny.ustawWartoscTasmy(1);
+
+        this.stanMaszyny.ustawPozycjeTasmy(0);
+        //          taśma      kod
+        //pozycja   0 1 2 3    0 1 2 3 4 5
+        //wartosc   1 1 1 0    [ [ - ] > ]
+        //wskaznik  ^          ^
+        
+        this.interpreter.krok();
+        System.out.println(this.stanMaszyny.pozycjaTasmy());
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        System.out.println(this.stanMaszyny.pozycjaTasmy());
+        this.interpreter.krok();
+        
+        
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        
+        
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+        this.interpreter.krok();
+      
+        assertTrue(this.stanMaszyny.wartoscTasmy() ==  0);
+        assertTrue(this.stanMaszyny.pozycjaTasmy() ==  3);
+        assertTrue(this.stanMaszyny.pozycjaKodu() == 0);
+    }
+    
 
 }
