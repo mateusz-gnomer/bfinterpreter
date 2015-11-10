65ef7fcfafe8536514fdcc4532a3dbdd7d239f1d
mateusz-gnomer <mateusz_gnomer@o2.pl>
2015-11-10 21:06:38 +0100

    Revert "Revert "Dodanie zwracania taśmy do stanu maszyny""
    This reverts commit a866f11448c5144170468a1d4505ac2adee0e217.

--- src/model/ProstyStanMaszyny.java
+++ src/model/ProstyStanMaszyny.java
@@ -55,6 +55,10 @@ public class ProstyStanMaszyny implements StanMaszyny {
         return this.dlugoscTasmy;
     };
     
+    public int[] zwrocTasme(){
+        return this.tasma;
+    }
+    
     public char aktualneWejscie(){
         if(this.wejscie.length()<=this.pozycjaWejscia ||
                 this.pozycjaWejscia<0){
